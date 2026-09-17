f8fb668c4768ead418e00bd4be72348557ee69b1
unomaill <uno.bro.bd@mohasin.bro.bd>
2026-09-17 23:37:09 +0600

    Update Firebase configuration with project details

--- firebase-config.js
+++ firebase-config.js
@@ -3,10 +3,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_FIREBASE_API_KEY",
-  authDomain: "REPLACE_WITH_PROJECT.firebaseapp.com",
-  projectId: "REPLACE_WITH_PROJECT_ID",
-  appId: "REPLACE_WITH_APP_ID",
+  apiKey: "AIzaSyDGyhEEjtbpcTVhmjkzfqzV72fZqayDb5c",
+  authDomain: "unomailbd.firebaseapp.com",
+  projectId: "unomailbd",
+  appId: "1:481222796028:web:41ecd7e95aefce12186d79",
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
